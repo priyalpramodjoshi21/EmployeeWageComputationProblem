a829fae3f5b80c944d8db98d813ca55013b3361f
“priyalpramodjoshi21” <priyalpramodjoshi@gmail.com>
2022-12-25 07:43:35 -0800

    Added: Calculate Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,9 +3,16 @@ echo "Welcome to Employee Wage Computation Program"
 ispresent=1;
 randomCheck=$((RANDOM%2));
 
-if [ $ispresent -eq $randomcheck ]
-then 
-       echo "Employee is present"
+if [ $ispresent -eq $randomCheck ]
+
+then
+ echo "Employee is present"
+empRatePerHr=20
+empHrs=8
+salary=$(($empHrs*$empRatePerHr))
+echo "salary="$salary
 else
        echo "Employee is Absent"
+       echo "salary="$salary
+
 fi
